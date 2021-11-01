c02480816d66ced125ab9b26275eba28d996dcb6
Huỳnh Văn Hữu Ân <19130003@st.hcmuaf.edu.vn>
2021-11-01 17:20:47 +0700

    Customer Controller - get list Customers

--- src/main/java/com/koi/springmvc/controller/CustomerController.java
+++ src/main/java/com/koi/springmvc/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.koi.springmvc.controller;
 
+import com.koi.springmvc.dao.CustomerDAO;
+import com.koi.springmvc.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    private CustomerDAO customerDAO;
+
+    @Autowired
+    public CustomerController(CustomerDAO customerDAO) {
+        this.customerDAO = customerDAO;
+    }
+
     @RequestMapping("/list")
     public String listCustomer(Model model) {
+        List<Customer> customers = customerDAO.getCustomers();
+
+        model.addAttribute("customers", customers);
+
         return "list-customers";
     }
 }
